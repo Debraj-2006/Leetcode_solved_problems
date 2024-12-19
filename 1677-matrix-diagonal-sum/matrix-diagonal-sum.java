8c5a6209306c77a036c77b421ed19b7a76b704e8
Debraj-2006 <ddebraj496@gmail.com>
2024-12-19 17:26:09 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (85.01%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+
+        // Loop through the matrix
+        for (int i = 0; i < n; i++) {
+            // Add primary diagonal element
+            sum += mat[i][i];
+
+            // Add secondary diagonal element, if not the same as primary diagonal element
+            if (i != n - i - 1) {
+                sum += mat[i][n - i - 1];
+            }
+        }
+
+        return sum;
+    }
+
+   
+}
